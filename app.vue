3a7bfbd0783bd4e54eddbd91b0b20950d7b4f734
Munsunty <munsunty@naver.com>
2023-06-07 00:27:48 +0900

    resume 최신 업데이트

--- app.vue
+++ app.vue
@@ -1,54 +1,346 @@
 <template>
   <div class="col-12 h-8rd shadow-2 p-3 h-full flex-column flex align-items-center justify-content-center surface-card" style="background-color: antiquewhite;">
-    <div class="col-10 grid" style="background-color:aquamarine">
-      <div class="col-4 p-2 flex align-items-center justify-content-center" style="background-color: brown;">
+    <div class="col-10 grid" >
+      <div class="col-4 p-2 flex align-items-center justify-content-center" >
         <img src="~/assets/self.jpg" alt="Image" id="selfy_img" class="selfy" height="300">
       </div>
-      <div class="col-8 p-2" style="background-color: red;">
+      <div class="col-8 p-2" >
         <ul>    
-                <li class="flex align-items-center mb-2">
-                  <div class="col-1">
-                    <img src="~/assets/assignment_ind_FILL0_wght400_GRAD0_opsz48.png" width="30">
-                  </div>
-                  <div class="col-9">
-                    <span class="p-2 text-900 font-bold text-4xl"> 강성민 (31)</span>
-                  </div>
-                </li>
-                <li class="flex align-items-center mb-1">
-                  <div class="col-1">
-                    <img src="~/assets/nurgul.png" width="36">
-                  </div>
-                  <div class="col-9">
-                    <span class="p-2 text-900  text-xl"> Dev_Nurgul</span>
-                  </div>
-                </li>
-                <li class="flex align-items-center mb-1">
-                  <div class="col-1">
-                    <img src="~/assets/terminal_FILL1_wght400_GRAD0_opsz48.png" width="36">
-                  </div>
-                  <div class="col-9">
-                    <span class="p-2 text-900  text-xl"> 2021.06~</span>
-                  </div>
-                </li>
-                <li class="flex align-items-center mb-1">
-                  <div class="col-1 flex align-items-center ml-1">
-                    <i class="pi pi-github" style="font-size: 30px"></i>
-                  </div>
-                  <div class="col-9">
-                    <span class="p-1 text-900  text-xl"> munsunty@naver.com</span>
-                  </div>
-                </li>
-                <li class="flex align-items-center mb-1">
-                  <div class="col-1 flex align-items-center">
-                    <img src="~/assets/send_FILL0_wght400_GRAD0_opsz48.png" width="36">
-                  </div>
-                  <div class="col-9">
-                    <span class="p-2 text-900  text-xl"> munsunty@naver.com</span>
-                  </div>
-                </li>
-            </ul>
+          <li class="flex align-items-center mb-2">
+            <div class="col-1">
+              <img src="~/assets/assignment_ind_FILL0_wght400_GRAD0_opsz48.png" width="30">
+            </div>
+            <div class="col-9">
+              <span class="p-2 text-900 font-bold text-4xl" style="color: #3c78d8 !important;"> 강성민 (31)</span>
+            </div>
+          </li>
+          <li class="flex align-items-center mb-1">
+            <div class="col-1">
+              <img src="~/assets/nurgul.png" width="36">
+            </div>
+            <div class="col-9">
+              <span class="p-2 text-900  text-xl"> Dev_Nurgul</span>
+            </div>
+          </li>
+          <li class="flex align-items-center mb-1">
+            <div class="col-1">
+              <img src="~/assets/terminal_FILL1_wght400_GRAD0_opsz48.png" width="36">
+            </div>
+            <div class="col-9">
+              <span class="p-2 text-900  text-xl"> 2021.06~</span>
+            </div>
+          </li>
+          <li class="flex align-items-center mb-1">
+            <div class="col-1 flex align-items-center ml-1">
+              <i class="pi pi-github" style="font-size: 30px"></i>
+            </div>
+            <div class="col-9">
+              <a href="https://github.com/Munsunty/nurgul" class="p-1 text-900  text-xl"> https://github.com/Munsunty</a>
+            </div>
+          </li>
+          <li class="flex align-items-center mb-1">
+            <div class="col-1 flex align-items-center">
+              <img src="~/assets/send_FILL0_wght400_GRAD0_opsz48.png" width="36">
+            </div>
+            <div class="col-9">
+              <a href="mailto:munsunty@naver.com" class="p-2 text-900  text-xl">munsunty@naver.com</a>
+            </div>
+          </li>
+        </ul>
       </div>
     </div>
+    <div class="col-10 grid mb-6" >
+      <div class="col-4 p-2 flex align-items-start justify-content-center" >
+        <span class=" text-900 text-bold text-4xl" style="color: #3c78d8 !important;">소개글</span>
+      </div>
+      <div class="col-8 p-2" >
+        웹어플리케이션 FullStack 개발자로서 역할을 하고 있습니다. Spring기반의 PLM 웹 어플리케이션을
+        JSTL, JPA, SSR, JQuery, JavaScript을 포함한 여러가지 도구를 사용하여 Framework를 확장중입니다.
+        프로그램 설계부터 프로세스 변경까지 전체적인 이해를 바탕으로 코딩을 수행합니다.
+        <br><br>
+        최신 기술에 관심이 많아, 새롭게 업데이트 되는 것들을 확인하고 어떻게 하면 업무에 적용할 수 있을지 고민합니다.
+        대학시절 미분방정식 수치해석을 위해서 mathlab으로 프로그래밍을 시작했고 필요에 따라 기술들을 익혀왔습니다.
+        공부하는 것을 즐기는 타입이라 러닝커브를 잘 극복하는 편입니다.
+        <br><br>
+        전공: 물리교육 --> 첫 직업: 물리교사  --> (현) 직업 : 프로그래머
+        <br>
+        Identity : 물리학자 + 과학자 + 프로그래머 + 교육자 + 엔지니어
+        <br>
+        Special : 과학적 기반 분석 + 검증 가능한 로직 설계/구현 + 프로세스 안정화  
+        <br><br>
+        단순히 잘하는 프로그래머가 아닌 인류에게 이로움을 주고 싶은 개발자가 되고자 합니다.<br>
+        그래서 오늘도 공부하고 도전하고 한 걸음씩 걸어갑니다. 
+      </div>
+    </div>
+    <div class="col-10 grid" >
+      <div class="col-4 p-2 flex align-items-start justify-content-center" >
+        <span class="text-900 text-bold text-4xl h-full" style="color: #3c78d8 !important;">SKILL</span>
+      </div>
+      <div class="col-8 p-2" >
+        <!-- <hr class="my-3 mx-0 border-top-1 border-none surface-border" /> -->
+        <div class="col-12 grid" style="margin-bottom: 10px;">
+            <div class="col-12 md:col-2">
+                <div  class="flex flex-column align-items-start justify-content-center h-full " >
+                    <div class="text-600 font-medium text-xl">
+                        Languge
+                    </div> 
+                </div>                            
+            </div>
+            <div class="col-12 md:col-6">
+                <img src="~/assets/icons8-javascript.gif">
+                <img src="~/assets/icons8-java.gif">
+                <img src="~/assets/icons8-c-48.png">
+                <img src="~/assets/icons8-python-48.png">
+            </div>
+        </div>
+        <hr class="my-3 mx-0 border-top-1 border-none surface-border" />
+        <div class="col-12 grid"  style="margin-bottom: 10px;">
+            <div class="col-12 md:col-2 ">
+                <div  class="flex flex-column align-items-start justify-content-center h-full " >
+                    <div class="text-600 font-medium text-xl ">
+                        FrameWork
+                    </div> 
+                </div>                             
+            </div>
+            <div class="col-12 md:col-7">
+                <img src="~/assets/icons8-nuxt-jc-48.png">
+                <img src="~/assets/icons8-vue-js-48.png">
+                <img src="~/assets/icons8-spring-boot-48.png">
+            </div>
+        </div>
+        <hr class="my-3 mx-0 border-top-1 border-none surface-border" />
+        <div class="col-12 grid"  style="margin-bottom: 10px;">
+            <div class="col-12 md:col-2 ">
+                <div  class="flex flex-column align-items-start justify-content-center h-full " >
+                    <div class="text-600 font-medium  text-xl">
+                        DataBase
+                    </div> 
+                </div>                             
+            </div>
+            <div class="col-12 md:col-7 ">
+                <img src="~/assets/icons8-oracle-logo-48.png" style="margin-right: 10px;">
+                <img src="~/assets/icons8-mysql-48.png">
+            </div>
+        </div>        
+      </div>
+    </div>
+    <div class="col-10 grid" >
+      <div class="col-4 p-2 flex align-items-start justify-content-center" >
+        <span class="text-900 text-bold text-4xl h-full" style="color: #3c78d8 !important;">EXPERIENCE</span>
+      </div>
+      <div class="col-8 p-2" >
+        <!-- <hr class="my-3 mx-0 border-top-1 border-none surface-border" /> -->
+        <div class="col-12 grid" style="margin-bottom: 10px;">
+            <div class="col-12 md:col-2">
+                <div  class="flex flex-column align-items-start justify-content-start h-full " >
+                    <div class="text-600 font-medium p-2">
+                        2021.06~
+                    </div> 
+                </div>                            
+            </div>
+            <div class="col-12 md:col-8">
+                <div class="col-12 font-bold text-2xl">CPMS</div>
+                <div class="col-12 text-500  ">개발자</div>
+                <div class="col-12  ">
+                  <ul>
+                    <li>Spring 기반 PLM 프로그램 CoreQ 솔루션 프로그램 개발 담당자</li>
+                    <li>Front FrameWork 개발 및 관리</li>
+                    <li>BackEnd Process 개발</li>
+                    <li>문서관리, 파일관리, 일정관리 프로세스 개발</li>
+                    <li>프로젝트 관리 프로그램 개발</li>
+                    <li>고객사 유지보수 담당</li>
+                    <li>유지보수 관련 문서 개발 및 관리</li>
+                    <li>프로그램 사용자 교육</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <hr class="my-3 mx-0 border-top-1 border-none surface-border" />     
+        <div class="col-12 grid" style="margin-bottom: 10px;">
+            <div class="col-12 md:col-2">
+                <div  class="flex flex-column align-items-start justify-content-start h-full " >
+                    <div class="text-600 font-medium p-2">
+                        2021.02
+                        <br>
+                        2020.03~
+                    </div> 
+                </div>                            
+            </div>
+            <div class="col-12 md:col-8">
+                <div class="col-12 font-bold text-2xl">보람중학교</div>
+                <div class="col-12 text-500  ">과학교사, 물리담당</div>
+                <div class="col-12  ">
+                  <ul>
+                    <li>1학년 담임</li>
+                    <li>과학이론 기반 실험위주 수업 진행</li>
+                    <li>학적 관리 담당</li>
+                    <li>코로나 비대면 수업 프로세스 구축 담당</li>
+                  </ul>
+                </div>
+            </div>
+        </div>
+        <hr class="my-3 mx-0 border-top-1 border-none surface-border" />
+        <div class="col-12 grid" style="margin-bottom: 10px;">
+            <div class="col-12 md:col-2">
+                <div  class="flex flex-column align-items-start justify-content-start h-full " >
+                    <div class="text-600 font-medium p-2">
+                        2019.02
+                        <br>
+                        2012.03~
+                    </div> 
+                </div>                            
+            </div>
+            <div class="col-12 md:col-8">
+                <div class="col-12 font-bold text-2xl">부산대학교</div>
+                <div class="col-12 text-500  ">물리교육과</div>
+                <div class="col-12  ">
+                  <ul>
+                    <li>졸업 - 4년</li>
+                    <li>현역 복무 - 2년</li>
+                    <li>해외 체류 - 1년</li>
+                    <li>학부연구생</li>
+                  </ul>
+                </div>
+            </div>
+        </div>
+      </div>
+    </div> 
+    <div class="col-10 grid" >
+      <div class="col-4 p-2 flex align-items-start justify-content-center" >
+        <span class="text-900 text-bold text-4xl h-full" style="color: #3c78d8 !important;">PROJECT</span>
+      </div>
+      <div class="col-8 p-2" >
+        <!-- <hr class="my-3 mx-0 border-top-1 border-none surface-border" /> -->
+        <div class="col-12 grid" style="margin-bottom: 10px;">
+            <div class="col-12 md:col-2">
+                <div  class="flex flex-column align-items-start justify-content-start h-full " >
+                    <div class="text-600 font-medium p-2">
+                        2023.01~
+                    </div> 
+                </div>                            
+            </div>
+            <div class="col-12 md:col-8">
+                <div class="col-12 font-bold text-2xl">신성 엔지니어링</div>
+                <div class="col-12 text-500  ">담당 개발자</div>
+                <div class="col-12  ">
+                  <ul>
+                    <li>PLM 프로그램 도입</li>
+                    <li>BackEnd 프로세스 업데이트 개발</li>
+                    <li>FrontEnd 프로세스 업데이트 개발</li>
+                    <li>사용자 요구사항에 따른 기능 업데이트</li>
+                    <li>DB I/F API 개발</li>
+                    <li>프로그램 사용자 교육</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <hr class="my-3 mx-0 border-top-1 border-none surface-border" />     
+        <div class="col-12 grid" style="margin-bottom: 10px;">
+            <div class="col-12 md:col-2">
+                <div  class="flex flex-column align-items-start justify-content-start h-full " >
+                    <div class="text-600 font-medium p-2">
+                        2023.02
+                        <br>
+                        2023.01~
+                    </div> 
+                </div>                            
+            </div>
+            <div class="col-12 md:col-8">
+                <div class="col-12 font-bold text-2xl">신규개발 프로젝트</div>
+                <div class="col-12 text-500  ">PM, 개발자</div>
+                <div class="col-12  ">
+                  <ul>
+                    <li>요청기반 ToDo List 설계~개발~구현</li>
+                    <li>이력관리 Item 바로비교 기능 설계</li>
+                    <li>채팅프로그램 도입 [웹소켓 기반]</li>
+                    <li>팀원들과의 화합, 신입 프로그래머 교육, 프로젝트 매니저 첫 수행</li>
+
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <hr class="my-3 mx-0 border-top-1 border-none surface-border" />
+        <div class="col-12 grid" style="margin-bottom: 10px;">
+            <div class="col-12 md:col-2">
+                <div  class="flex flex-column align-items-start justify-content-start h-full " >
+                    <div class="text-600 font-medium p-2">
+                        2022.12
+                        <br>
+                        2022.05~
+                    </div> 
+                </div>                            
+            </div>
+            <div class="col-12 md:col-8">
+                <div class="col-12 font-bold text-2xl">SL전자 프로젝트</div>
+                <div class="col-12 text-500  ">FullStack 개발자</div>
+                <div class="col-12  ">
+                  <ul>
+                    <li>WIJMO 기반 Grid Edit 화면 개발</li>
+                    <li>신규 데이터 검색화면 개발</li>
+                    <li>실시간 데이터 편집 Validation API 개발</li>
+                    <li>XML 기반 UI 자동생성 API 개발</li>
+                    <li>사용자 기반 Item 검색 API 개발</li>
+                    <li>Merged Tree Data 데이터 모델링 및 API 개발</li>
+                    <li>기존 데이터 검색속도 대비 7배 개선 로직 개발</li>
+                  </ul>
+                </div>
+            </div>
+        </div>
+        <hr class="my-3 mx-0 border-top-1 border-none surface-border" />  
+        <div class="col-12 grid" style="margin-bottom: 10px;">
+            <div class="col-12 md:col-2">
+                <div  class="flex flex-column align-items-start justify-content-start h-full " >
+                    <div class="text-600 font-medium p-2">
+                        2022.05
+                        <br>
+                        2022.02~
+                    </div> 
+                </div>                            
+            </div>
+            <div class="col-12 md:col-8">
+                <div class="col-12 font-bold text-2xl">Seegen</div>
+                <div class="col-12 text-500  ">담당 개발자</div>
+                <div class="col-12  ">
+                  <ul>
+                    <li>Project Management Software 도입</li>
+                    <li>사용자 요구사항에 맞는 Project Role 기반 프로세스 개발</li>
+                    <li>소프트웨어 사용 교육 및 UI 개선</li>
+                    <li>신약 개발에 필요한 업무 프로세스 진행이 가능해짐.</li>
+                  </ul>
+                </div>
+            </div>
+        </div>
+        <div class="col-12 grid" style="margin-bottom: 10px;">
+            <div class="col-12 md:col-2">
+                <div  class="flex flex-column align-items-start justify-content-start h-full " >
+                    <div class="text-600 font-medium p-2">
+                        2022.02
+                        <br>
+                        2021.06~
+                    </div> 
+                </div>                            
+            </div>
+            <div class="col-12 md:col-8">
+                <div class="col-12 font-bold text-2xl">귀뚜라미</div>
+                <div class="col-12 text-500  ">담당 개발자</div>
+                <div class="col-12  ">
+                  <ul>
+                    <li>PLM 프로그램 CoreQ 솔루션 도입</li>
+                    <li>사용자 요구사항에 맞는 화면 UI 개발 및 업데이트</li>
+                    <li>BackEnd 프로세스 개발 </li>
+                    <li>전자결제 API 개선</li>
+                    <li>데이터 마이그레이션 프로그램 개발</li>
+                    <li>DB I/F API 개발</li>
+                    <li>프로그램 사용자 교육 [200명]</li>
+                    <li>프로그램 도입 이후 데이터 정합성 및 안정성이 대폭 강화됨.</li>
+                    <li>제품개발 업무의 수준이 시스템 기반으로 변경됨.</li>
+                  </ul>
+                </div>
+            </div>
+        </div>
+        <hr class="my-3 mx-0 border-top-1 border-none surface-border" />  
+      </div>
+    </div>  
   
   </div>
 </template>
